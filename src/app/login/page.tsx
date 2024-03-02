71485e4a89a2b66dcd98e3bf2f7c3037d26d4b82
Prakhar <100105525+prak132@users.noreply.github.com>
2024-03-02 11:35:41 -0800

    theme support and autocomplete

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,10 +1,13 @@
 "use client";
 import { useState } from 'react';
-import { UserCircleIcon, LockClosedIcon } from "@heroicons/react/24/solid";
+import { UserCircleIcon, EyeIcon, EyeSlashIcon, LockClosedIcon, ArrowLongRightIcon } from "@heroicons/react/24/solid";
+import useTheme from "@/app/components/utils/theme/updateTheme";
 
 export default function Login() {
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
+    const [showPassword, setShowPassword] = useState(false);
+    const [theme, setTheme] = useTheme();
 
     const handleSubmit = (e) => {
         e.preventDefault();
@@ -14,7 +17,7 @@ export default function Login() {
     return (
         <div className="flex justify-center items-center h-screen font-inter">
             <div className="w-[450px] p-6">
-                <h2 className="text-black text-left text-3xl font-semibold mb-2">Log In</h2>
+                <h2 className="text-black dark:text-white text-left text-3xl font-semibold mb-2">Log In</h2>
                 <p className="text-gray-400 text-left text-sm mb-4">
                     Don&apos;t have an account?{' '}
                     <a className="underline text-blue-500" href='/signup'>Sign Up</a>
@@ -27,24 +30,29 @@ export default function Login() {
                             value={username}
                             onChange={(e) => setUsername(e.target.value)}
                             required
-                            className="w-full px-6 py-[14px] pl-12 rounded filter drop-shadow-md text-black text-sm"
+                            autoComplete="email"
+                            className="w-full px-6 py-[14px] pl-12 rounded-lg filter drop-shadow-md text-black text-sm"
                         />
                         <UserCircleIcon className="absolute left-4 top-1/2 transform -translate-y-1/2 w-6 h-6 text-gray-400" />
                     </div>
                     <div className="relative mb-2">
                         <input
-                            type="password"
+                            type={showPassword ? "text" : "password"}
                             placeholder="Password"
                             value={password}
                             onChange={(e) => setPassword(e.target.value)}
                             required
-                            className="w-full px-6 py-[14px] pl-12 rounded filter drop-shadow-md text-black text-sm"
+                            autoComplete="current-password"
+                            className="w-full px-6 py-[14px] pl-12 pr-12 rounded-lg filter drop-shadow-md text-black text-sm"
                         />
                         <LockClosedIcon className="absolute left-4 top-1/2 transform -translate-y-1/2 w-6 h-6 text-gray-400" />
-                    </div>
+                        <div className="absolute right-4 top-1/2 transform -translate-y-1/2 cursor-pointer" onClick={() => setShowPassword(!showPassword)}>
+                            {showPassword ? <EyeSlashIcon className="w-6 h-6 text-gray-400" /> : <EyeIcon className="w-6 h-6 text-gray-400" />}
+                        </div>
+                    </div>  
                     <a className="text-blue-500 text-sm" href='https://google.com/'>Forgot Password?</a>
-                    <button type="submit" className="w-full flex items-center justify-center bg-blue-500 filter drop-shadow-md text-white px-4 py-3 rounded cursor-pointer text-base">
-                        Log In
+                    <button type="submit" className="w-full flex items-center justify-center bg-blue-500 filter drop-shadow-md text-white px-4 py-3 rounded-lg cursor-pointer text-base">
+                        Log In <ArrowLongRightIcon className="ml-4 w-6 h-6" />
                     </button>
                 </form>
             </div>
--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,12 +1,14 @@
 "use client";
 import { useState } from 'react';
 import { ArrowLongRightIcon, AtSymbolIcon, UserCircleIcon, LockClosedIcon } from "@heroicons/react/24/solid";
+import useTheme from "@/app/components/utils/theme/updateTheme";
 
 export default function Signup() {
     const [email, setEmail] = useState('');
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
     const [confirmPassword, setConfirmPassword] = useState('');
+    const [theme, setTheme] = useTheme();
 
     const handleSubmit = (e) => {
         e.preventDefault();
@@ -15,7 +17,7 @@ export default function Signup() {
     return (
         <div className="flex justify-center items-center h-screen font-inter">
             <div className="w-[450px] p-6">
-                <h2 className="text-black text-left text-3xl font-semibold mb-2">Sign Up</h2>
+                <h2 className="text-black dark:text-white text-left text-3xl font-semibold mb-2">Sign Up</h2>
                 <p className="text-gray-400 text-left text-sm mb-4">
                     Already have an account?{' '}
                     <a className="underline text-blue-500" href='https://google.com/'>Login</a>
@@ -28,7 +30,8 @@ export default function Signup() {
                             value={email}
                             onChange={(e) => setEmail(e.target.value)}
                             required
-                            className="w-full px-6 py-[14px] pl-12 rounded filter drop-shadow-md text-black text-sm"
+                            autoComplete="email"
+                            className="w-full px-6 py-[14px] pl-12 rounded-lg filter drop-shadow-md text-black text-sm"
                         />
                         <AtSymbolIcon className="absolute left-4 top-1/2 transform -translate-y-1/2 w-6 h-6 text-gray-400" />
                     </div>
@@ -39,33 +42,36 @@ export default function Signup() {
                             value={username}
                             onChange={(e) => setUsername(e.target.value)}
                             required
-                            className="w-full px-6 py-[14px] pl-12 rounded filter drop-shadow-md text-black text-sm"
+                            autoComplete="off"
+                            className="w-full px-6 py-[14px] pl-12 rounded-lg filter drop-shadow-md text-black text-sm"
                         />
                         <UserCircleIcon className="absolute left-4 top-1/2 transform -translate-y-1/2 w-6 h-6 text-gray-400" />
                     </div>
                     <div className="relative mb-[10px]">
                         <input
-                            type="password"
+                            type="text"
                             placeholder="Password"
                             value={password}
                             onChange={(e) => setPassword(e.target.value)}
                             required
-                            className="w-full px-6 py-[14px] pl-12 rounded filter drop-shadow-md text-black text-sm"
+                            autoComplete="new-password"
+                            className="w-full px-6 py-[14px] pl-12 rounded-lg filter drop-shadow-md text-black text-sm"
                         />
                         <LockClosedIcon className="absolute left-4 top-1/2 transform -translate-y-1/2 w-6 h-6 text-gray-400" />
                     </div>
                     <div className="relative mb-[10px]">
                         <input
-                            type="password"
+                            type="text"
                             placeholder="Confirm Password"
                             value={confirmPassword}
                             onChange={(e) => setConfirmPassword(e.target.value)}
                             required
-                            className="w-full px-6 py-[14px] pl-12 rounded filter drop-shadow-md text-black text-sm"
+                            autoComplete="new-password"
+                            className="w-full px-6 py-[14px] pl-12 rounded-lg filter drop-shadow-md text-black text-sm"
                         />
                         <LockClosedIcon className="absolute left-4 top-1/2 transform -translate-y-1/2 w-6 h-6 text-gray-400" />
                     </div>
-                    <button type="submit" className="w-full flex items-center justify-center bg-blue-500 filter drop-shadow-md text-white px-4 py-3 rounded mb-[10px] cursor-pointer text-base">
+                    <button type="submit" className="w-full flex items-center justify-center bg-blue-500 filter drop-shadow-md text-white px-4 py-3 rounded-lg mb-[10px] cursor-pointer text-base">
                         Signup <ArrowLongRightIcon className="ml-4 w-6 h-6" />
                     </button>
                 </form>
